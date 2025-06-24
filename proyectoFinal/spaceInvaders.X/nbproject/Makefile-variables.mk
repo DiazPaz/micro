483351f4258e4384b46c2bd9e50eb51b5996aa0f
David D. <daizpaz19@gmail.com>
2025-06-24 17:35:09 -0600

    Creación de proyecto final e implementación de libreriías lcd.c y lcd.h. Se copió el main de testPr2.

---
+++ proyectoFinal/spaceInvaders.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=spaceInvaders.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/spaceInvaders.X.production.hex
